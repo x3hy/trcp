070e83469acc6dcbf2dc33bec6ecfe1f1c8728d4
9jh1 <tkf.x1os@gmail.com>
2025-12-05 12:52:23 +1300

    auto_commit_Fri Dec  5 12:52:23 PM NZDT 2025

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
@@ -11,8 +12,11 @@ typedef struct {
 } message;
 
 typedef struct {
-	char *server_ip;
-	char *endpoint;
+	struct {
+		char *ip;
+		int port;
+		char *endpoint;
+	} server;
 	char *username;
 } app_config;
 
@@ -21,21 +25,27 @@ static char *get_time(void);
 static message msg_init(char *);
 static void msg_print(message);
 static void msg_free(message *);
+static char *msg_url(message); 
+static char *server_url(app_config);
+static void free_app_config(app_config);
 
-static app_config app;
+static app_config base;
 
 int 
 main()
 {
 	// Init app 
-	app.username = strdup("tes123");
+	base.username = strdup("tes123");
+	base.server.ip = strdup("http://127.0.0.1:5511");
+	base.server.endpoint = strdup("/msg");
 
 	// Init message
 	message test = msg_init("test123");
 	msg_print(test);
+
 	msg_free(&test);
 	return 0;
-}	
+}
 
 
 // Returns UTC ISO date
@@ -61,19 +71,19 @@ get_time(void)
 
 
 // Initializes a message 
-message
+static message
 msg_init(char *msg)
 {
 	message out;
 	out.message = strdup(msg);
 	out.time = get_time();
-	out.username = app.username;
+	out.username = base.username;
 	return out;
 }
 
 
 // Print a messages values 
-void 
+static void 
 msg_print(message msg)
 {
 	printf("message: %s\ntime: %s\nusername: %s\n",
@@ -82,11 +92,65 @@ msg_print(message msg)
 			msg.username);
 }
 
+
 // Free message data 
-void
+static void
 msg_free(message *msg)
 {
 	free(msg->message);
 	free(msg->time);
 	free(msg->username);
-} 
+}
+
+// Get the full server URL string
+static char *
+server_url(app_config app)
+{
+	char *out;
+	size_t out_s = snprintf(NULL, 0, "%s:%d/%s",
+			app.server.ip,
+			app.server.port,
+			app.server.endpoint);
+
+	out = malloc(out_s + 1);
+
+	snprintf(out, out_s, "%s:%d/%s", 
+			app.server.ip,
+			app.server.port,
+			app.server.endpoint);
+
+	return out;
+}
+
+// Generate a url for posting message to server 
+static char *
+msg_url(message msg)
+{
+	char *out;
+
+	char *serv = server_url(base);
+	size_t out_s = snprintf(NULL, 0, "%s/%s/%s/%s",
+			serv,
+			msg.username,
+			msg.time,
+			msg.message);
+
+	out = malloc(out_s + 1);
+
+	snprintf(out, out_s, "%s/%s/%s/%s",
+			serv,
+			msg.username,
+			msg.time,
+			msg.message);
+
+	return out;
+}
+
+// Free app_config memory 
+static void 
+free_app_config(app_config app)
+{
+	free(app.username);
+	free(app.server.ip);
+	free(app.server.endpoint);
+}
