ee26b637106d8c34550041f0fa3927b567b1427b
9jh1 <tkf.x1os@gmail.com>
2025-12-05 13:25:42 +1300

    auto_commit_Fri Dec  5 01:25:42 PM NZDT 2025

--- main.c
+++ main.c
@@ -28,12 +28,21 @@ static void msg_free(message *);
 static char *msg_url(message); 
 static char *server_url(app_config);
 static void free_app_config(app_config);
+static char* base64_decode(unsigned char *, const size_t);
 static char* base64_encode(const unsigned char *, size_t);
 
 		
 static app_config base;
 static const unsigned char base64_table[65] = 
 	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+static const int B64index[256] = { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 62, 63, 62, 62, 63, 52, 53, 54, 55,
+56, 57, 58, 59, 60, 61,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,
+7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,  0,
+0,  0,  0, 63,  0, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51 };
+
 
 int 
 main()
@@ -57,6 +66,8 @@ main()
 	char* base64 = base64_encode((const unsigned char *)"test123", 8);
 	printf("%s\n",base64);
 	free(base64);
+
+	char *decoded_base64 = base64_decode((unsigned char *)base64, strlen(base64)); 
 
 	msg_free(&test);
 	return 0;
@@ -219,3 +230,40 @@ base64_encode(const unsigned char *src, size_t len)
 
 	return outStr;
 }
+
+// https://stackoverflow.com/questions/180947/base64-decode-snippet-in-c/13935718
+char* 
+base64_decode(unsigned char *p, const size_t len)
+{
+	int pad = len > 0 && (len % 4 || p[len -1] == '=');
+	const size_t L = ((len + 3) / 4 - pad) * 4;
+	const size_t str_size = L / 4 * 3 + pad;
+	char *str = malloc(str_size + 1);
+
+	for(size_t i = 0, j = 0; i < L; i += 4)
+	{
+		int n = B64index[p[i]] << 18 | 
+						B64index[p[i + 1]] << 12 | 
+						B64index[p[i + 2]] << 6 | 
+						B64index[p[i + 3]];
+
+		str[j++] = n >> 16;
+		str[j++] = n >> 8 & 0xFF;
+		str[j++] = n & 0xFF;
+	}
+
+	if (pad)
+	{
+		int n = B64index[p[L]] << 18 |
+						B64index[p[L + 1]] << 12;
+		str[str_size - 1] = n >> 16;
+
+		if (len > L + 2 && p[L + 2] != '=')
+		{
+			n |= B64index[p[L + 2]] << 6;
+			str[str_size + 1] = (n >> 8 & 0xFF);
+		}
+	}
+
+	return str;
+} 
