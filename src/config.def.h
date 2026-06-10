06e6dfb288973f1e9f97adf578705b936ea55513
x3hy <tkf.x1os@gmail.com>
2026-06-10 09:41:06 +0000

    Added new options

--- src/config.def.h
+++ src/config.def.h
@@ -1,11 +1,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H
+#define PORT_MAX     65535 // maximum port size
+#define PORT_MIN     1024  // minimum port size
 
-/* server settings */
-#define PORT_MAX 65535
-#define PORT_MIN 1024
+/* Server settings */
 #define DEFAULT_PORT 6060
-#define BUFFER_SIZE 512 // max return size
-#define MAX_CLIENTS 10  // max amount of connections
-
+#define MAX_THREADS  10    // max amount of connections
+#define MAX_MSG_SIZE 512   // max string length of messages sent to the server
+#define BACKLOG_SIZE 10    // amount of items available in the backlog
 #endif
